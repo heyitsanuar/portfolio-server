a5efbd009c8db151c2031321392644a68fce20a9
Anuar Jimenez <=>
2019-07-21 16:27:38 -0500

    [STRUCTURE] [ADD] Initial structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,3 @@
 import express from 'express';
-import { BagRoutes } from '@bag/bag.routes';
-import { ProductRoutes } from '@product/product.routes';
 
 export const AppRoutes = express.Router();
-
-AppRoutes.use(BagRoutes);
-AppRoutes.use(ProductRoutes);
